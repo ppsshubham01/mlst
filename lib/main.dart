b8ac98c0106c6e7a180fb1185635261b9a5d9e31
Sagar Patel <sagar.patel@sayalife.in>
2024-04-15 16:22:48 +0530

    fisr

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mlit_basic_flutter/screens/home_screens.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
